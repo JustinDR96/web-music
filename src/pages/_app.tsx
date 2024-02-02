2717d13549254dc62379f24d57f6fc1522273923
De Roover Justin <justinderoover@gmail.com>
2024-02-03 00:52:49 +0100

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
-import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
